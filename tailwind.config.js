6e5f5e78f9d216b9c9edd4c16aad46b9bd787946
Andres Fernando <andres.fernando08@gmail.com>
2020-12-30 09:15:11 -0500

    manejo imagenes responsivas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         'primary': '#f3c614',
         'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem', // 80 * 0.25
+        '100': '25rem' // 100 * 0.25
       }
     },
   },
